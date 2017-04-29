f6e18b371da5c31ceb1760da114a621831d5cdb9
mqtran01 <michaelqtran01@gmail.com>
2017-04-30 04:32:25 +1000

    test of posting

--- app/views.py
+++ app/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+import cv2
 
 def index(request):
 	#template = loader.get_template('app/index.html')
 	#return HttpResponse("<h1>Hello world!</h1>")
-
-
-
 	
 	template = loader.get_template('app/index.html')
 	
@@ -20,3 +18,9 @@ def index(request):
 def test(request):
 	template = loader.get_template('app/test.html')
 	return HttpResponse(template.render(None, request))
+
+def upload(request, image):
+	if request.POST:
+
+	else:	
+		return HttpResponse("good try.")
